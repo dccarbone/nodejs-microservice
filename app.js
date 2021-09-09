69595e10f1b6816f9935318ad2a7469946e57e6f
Diego Cavichiolli Carbone <dccarbone.app@gmail.com>
2021-09-09 04:04:37 -0300

    Adding drone file

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 9090
+
+app.get('/api', (req, res) =>{
+    res.send('hello!')
+})
+
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}`)
+})
